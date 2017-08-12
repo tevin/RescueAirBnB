a1424e9a55bc2c27a67466b701a7d9dd375c6236
Tevin <motevin@gmail.com>
2017-08-12 17:00:42 -0400

    http auth for hosts

--- app.py
+++ app.py
@@ -1,40 +1,59 @@
 from flask import Flask, request, render_template
 from pymongo import MongoClient
 from envparse import env
+from flask_httpauth import HTTPDigestAuth
 import os.path
 
-app = Flask(__name__)
 
-
-def mongo_login():
+# Get env vars stored either in an env file or on the machine
+def get_env(name):
 	if (os.path.exists('./env')):
 		env.read_envfile('./env')
-	mongo_uri = env('MONGO_URI')
+	return env(name)
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = get_env('SECRET_KEY')
+users = users = {
+    "admin": get_env('ADMIN_PASS')
+}
+auth = HTTPDigestAuth()
+
+@auth.get_password
+def get_pw(username):
+    if username in users:
+        return users.get(username)
+    return None
+
+
+# Utility method for mongo connections
+def mongo_login():
+	mongo_uri=get_env('MONGO_URI')
 	client = MongoClient(mongo_uri)
 	return client['rescuebnb']
 
+# Home page with host form
 @app.route('/')
 def show_home():
-    return render_template('addhosts.html')
+	return render_template('addhosts.html')
 
-@app.route('/hosts', methods = ['GET', 'POST'])
+# Post endpoint for committing host to db
+@app.route('/addhost', methods = ['POST'])
 def hosts():
-	if request.method == 'POST':
-		add_host(request.form.to_dict())
-		return render_template('addhosts.html')
-	else:
-		return render_template('viewhosts.html', hosts=get_hosts())
-
-def add_host(host):
 	db = mongo_login()
-	hosts = db.hosts
-	host_id = hosts.insert_one(host).inserted_id
+	hosts_collection = db.hosts
+	host = request.form.to_dict()
+	hosts_collection.insert_one(host) # should probably check for completed insert
+	
+	return render_template('addhosts.html')
 
-def get_hosts():
+# "Secured" endpoint for viewing registered hosts
+@app.route('/hosts')
+@auth.login_required
+def viewhosts():
 	db = mongo_login()
-	hosts = db.hosts
-	return list(hosts.find())
+	hosts_collection = db.hosts
+	return render_template('viewhosts.html', hosts=list(hosts_collection.find()))
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
     #app.run(debug=True)
